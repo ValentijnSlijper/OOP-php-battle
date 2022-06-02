182d008f1d0a98d881d4a780ffef4b50a49a492e
Valentijn <99057635@mydavinci.nl>
2022-06-02 14:24:36 +0200

    Added all classes

---
+++ pokemon.php
@@ -0,0 +1,13 @@
+<?php
+
+ class Pokemon{
+      public $name;
+      public $enerytype;
+      public $hitpoints;
+      public $health;
+      public $attack;
+      public $weakness;
+      public $resistance;
+ }
+
+ ?>
